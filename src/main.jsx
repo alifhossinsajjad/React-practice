c9b90f1ee013881348b0edf70836aef5b2917298
MD ALIF MIA <alifhossinsajjad123456@gmail.com>
2025-09-17 23:19:34 +0600

    fix the function bug

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Counter from './counter.jsx'
+import Sports from './Sports.jsx'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
   <Counter/>
+  <Sports/>
   </StrictMode>,
 )
